d4e865c9df9a53b67e4876b70fb1311d6b30aee3
obillekyle <obillekyle@gmail.com>
2024-10-12 23:51:27 +0800

    refactor(lrc-parser): optimize code

--- src/api/parser.ts
+++ src/api/parser.ts
@@ -1,300 +1,313 @@
-import { CustomEventHandler } from './event';
-import { getUnique } from './util';
+import {
+	normalizeNewLines,
+	is,
+	CustomEventHandler,
+	getUnique,
+} from "@vue-material/core";
 
 export type LRCTags = Record<string, string>;
 
 export type LRCLine =
-  | {
-      id: number;
-      type: 'single';
-      time: number;
-      data: string;
-    }
-  | {
-      id: number;
-      type: 'multi';
-      time: number;
-      data: {
-        offset: number;
-        line: string;
-      }[];
-    };
+	| {
+			id: number;
+			type: "single";
+			time: number;
+			data: string;
+	  }
+	| {
+			id: number;
+			type: "multi";
+			time: number;
+			data: {
+				offset: number;
+				line: string;
+			}[];
+	  };
 
 export type LRCData = {
-  tags: LRCTags;
-  lines: LRCLine[];
+	tags: LRCTags;
+	lines: LRCLine[];
 };
 
 export type LRCEvents = {
-  parsed: undefined;
-  'tag-added': [key: string, value: string];
-  'tag-update': [key: string, value: string, oldValue: string];
-  'tag-removed': [key: string, value: string];
+	parsed: undefined;
+	"tag-added": [key: string, value: string];
+	"tag-update": [key: string, value: string, oldValue: string];
+	"tag-removed": [key: string, value: string];
 
-  'line-updated': [index: number, data: LRCLine];
-  'line-added': [index: number, data: LRCLine];
-  'line-removed': [index: number, data: LRCLine];
+	"line-updated": [index: number, data: LRCLine];
+	"line-added": [index: number, data: LRCLine];
+	"line-removed": [index: number, data: LRCLine];
 };
 
 export class LRCParser extends CustomEventHandler<LRCEvents> {
-  private _cachedTime: number[] = [];
-  public tags: LRCTags = {};
-  public lines: LRCLine[] = [];
-  get EMPTYLINE(): LRCLine {
-    return {
-      id: getUnique(),
-      type: 'single',
-      time: 0,
-      data: '',
-    };
-  }
+	private _cachedTime: number[] = [];
+	public tags: LRCTags = {};
+	public lines: LRCLine[] = [];
+	get EMPTYLINE(): LRCLine {
+		return {
+			id: getUnique(),
+			type: "single",
+			time: 0,
+			data: "",
+		};
+	}
 
-  constructor(lrc?: string) {
-    super();
+	constructor(lrc?: string) {
+		super();
 
-    lrc && this.parse(lrc);
-  }
+		lrc && this.parse(lrc);
+	}
 
-  getRaw(): LRCData {
-    return {
-      tags: { ...this.tags },
-      lines: [...this.lines],
-    };
-  }
+	getRaw(): LRCData {
+		return {
+			tags: { ...this.tags },
+			lines: [...this.lines],
+		};
+	}
 
-  getJSON(): string {
-    return JSON.stringify(this.getRaw());
-  }
+	getJSON(): string {
+		return JSON.stringify(this.getRaw());
+	}
 
-  parse(data: string) {
-    this.reset();
-    const lines = data.split('\n');
-    for (const line of lines) {
-      const match = line.match(/\[(\d{2}:\d{2}\.\d{2,3})\](.*)|\[(.*)\]/);
-      if (match) {
-        let [, timeString, data, tag] = match;
-        if (tag) {
-          const tagMatch = tag.match(/(.*)\:(.*)/);
-          if (tagMatch) this.tags[tagMatch[1]] = tagMatch[2];
-          continue;
-        }
+	private processTag(tag: string) {
+		const match = tag.match(/(.*)\:(.*)/);
 
-        data = data.trim();
-        const time = this.timeToNumber(timeString!);
-        const multiMatch = data.match(/(.*)(\[\d{2}:\d{2}.\d{2,3}\].*)/);
+		if (match) {
+			this.tags[match[1]] = match[2];
+		}
+	}
 
-        if (!multiMatch) {
-          this.lines.push({ id: getUnique(), type: 'single', time, data });
-          continue;
-        }
+	private processLine(timeStr: string, line: string) {
+		const time = LRCParser.timeToInt(timeStr);
+		const value = line.trim();
 
-        const lyric: LRCLine = {
-          id: getUnique(),
-          type: 'multi',
-          time: time,
-          data: [],
-        };
+		const multiMatch = value.match(/(.*)(\[\d{2}:\d{2}.\d{2,3}\].*)/);
 
-        const [, firstString, multi] = multiMatch;
-        lyric.data.push({ offset: 0, line: firstString || '' });
+		if (multiMatch) {
+			const [, firstString, multi] = multiMatch;
 
-        const multiLines = multi.split('[');
-        for (const multiLine of multiLines) {
-          const array = multiLine.match(/(\d{2}:\d{2}.\d{2,3})\](.*)/);
-          if (array) {
-            const [, timeRaw, line] = array;
-            const offset = this.timeToNumber(timeRaw) - time;
-            lyric.data.push({ offset, line });
-          }
-        }
-      }
+			const lyric: LRCLine = {
+				id: getUnique(),
+				type: "multi",
+				time: time,
+				data: [],
+			};
 
-      this.lines.push({
-        id: getUnique(),
-        type: 'single',
-        time: 0,
-        data: line,
-      });
-    }
+			lyric.data.push({ offset: 0, line: firstString || "" });
 
-    this.updateCachedTime();
-    this.dispatchEvent('parsed', undefined);
-  }
+			const multiLines = multi.split("[");
+			for (const multiLine of multiLines) {
+				const array = multiLine.match(/(\d{2}:\d{2}.\d{2,3})\](.*)/);
+				if (array) {
+					const [, timeRaw, line] = array;
+					const offset = LRCParser.timeToInt(timeRaw) - time;
+					lyric.data.push({ offset, line });
+				}
+			}
 
-  stringify(): string {
-    let first = true;
-    let finalString = '';
-    for (const tagName in this.tags) {
-      finalString += first ? '' : '\n';
-      finalString += `[${tagName}:${this.tags[tagName]}]`;
-      first = false;
-    }
-    for (const line of this.lines) {
-      finalString += first ? '' : '\n';
-      first = false;
+			return;
+		}
 
-      const time = this.timeToString(line.time);
-      if (line.type === 'single') {
-        finalString += `[${time}] ${line.data}`;
-      }
+		this.lines.push({
+			id: getUnique(),
+			type: "single",
+			time,
+			data: value,
+		});
+	}
 
-      if (line.type === 'multi') {
-        let concat = `[${time}] `;
+	parse(data: string) {
+		this.reset();
+		const lines = normalizeNewLines(data).split("\n");
 
-        for (const data of line.data) {
-          if ((data.offset = 0)) {
-            concat += data.line;
-            continue;
-          }
-          const multiTime = this.timeToString(line.time + data.offset);
-          concat += `[${multiTime}]${data.line}`;
-        }
-        finalString += concat;
-      }
-    }
-    return finalString;
-  }
+		for (const line of lines) {
+			const match = line.match(/\[(\d{2}:\d{2}\.\d{2,3})\](.*)|\[(.*)\]/);
 
-  updateTag(key: string, value: string) {
-    for (const tagName in this.tags) {
-      if (tagName === key) {
-        this.dispatchEvent('tag-update', [key, value, this.tags[tagName]]);
-        this.tags.value = value;
-        return;
-      }
-    }
-  }
+			if (match) {
+				const [, timeString, value, tag] = match;
+				tag ? this.processTag(tag) : this.processLine(timeString, value);
+			}
+		}
 
-  addTag(key: string, value: string) {
-    if (this.tags[key]) return;
+		this.updateCachedTime();
+		this.dispatchEvent("parsed");
+	}
 
-    this.tags[key] = value;
-    this.dispatchEvent('tag-added', [key, value]);
-  }
+	stringify(): string {
+		let first = true;
+		let finalString = "";
+		for (const tagName in this.tags) {
+			finalString += first ? "" : "\n";
+			finalString += `[${tagName}:${this.tags[tagName]}]`;
+			first = false;
+		}
+		for (const line of this.lines) {
+			finalString += first ? "" : "\n";
+			first = false;
 
-  removeTag(key: string) {
-    for (const tagName in this.tags) {
-      if (tagName === key) {
-        this.dispatchEvent('tag-removed', [key, this.tags[tagName]]);
-        delete this.tags[key];
-        return;
-      }
-    }
-  }
+			const time = this.timeToString(line.time);
+			if (line.type === "single") {
+				finalString += `[${time}] ${line.data}`;
+			}
 
-  import(data: LRCData) {
-    this.tags = { ...data.tags };
-    this.lines = [...data.lines];
+			if (line.type === "multi") {
+				let concat = `[${time}] `;
 
-    this.updateCachedTime();
-    this.dispatchEvent('parsed', undefined);
-  }
+				for (const data of line.data) {
+					if (data.offset === 0) {
+						concat += data.line;
+						continue;
+					}
+					const multiTime = this.timeToString(line.time + data.offset);
+					concat += `[${multiTime}]${data.line}`;
+				}
+				finalString += concat;
+			}
+		}
+		return finalString;
+	}
 
-  updateCachedTime() {
-    this._cachedTime.length = 0;
-    for (const line of this.lines) {
-      this._cachedTime.push(line.time);
-    }
-  }
+	updateTag(key: string, value: string) {
+		for (const tagName in this.tags) {
+			if (tagName === key) {
+				this.dispatchEvent("tag-update", key, value, this.tags[tagName]);
+				this.tags.value = value;
+				return;
+			}
+		}
+	}
 
-  updateLine(index: number, line: Omit<Partial<LRCLine>, 'id'>) {
-    'id' in line && delete line.id;
+	addTag(key: string, value: string) {
+		if (this.tags[key]) return;
 
-    Object.assign(this.lines[index], line);
-    this._cachedTime[index] = this.lines[index].time;
+		this.tags[key] = value;
+		this.dispatchEvent("tag-added", key, value);
+	}
 
-    this.dispatchEvent('line-updated', [index, this.lines[index]]);
-  }
+	removeTag(key: string) {
+		for (const tagName in this.tags) {
+			if (tagName === key) {
+				this.dispatchEvent("tag-removed", key, this.tags[tagName]);
+				delete this.tags[key];
+				return;
+			}
+		}
+	}
 
-  removeLine(index: number) {
-    const line = this.lines[index];
-    this.lines.splice(index, 1);
-    this._cachedTime.splice(index, 1);
+	import(data: LRCData) {
+		this.tags = { ...data.tags };
+		this.lines = [...data.lines];
 
-    this.dispatchEvent('line-removed', [index, line]);
-  }
+		this.updateCachedTime();
+		this.dispatchEvent("parsed");
+	}
 
-  removeFromId(id: number) {
-    const index = this.lines.findIndex((line) => line.id === id);
-    if (index !== -1) {
-      this.removeLine(index);
-    }
+	updateCachedTime() {
+		this._cachedTime.length = 0;
+		for (const line of this.lines) {
+			this._cachedTime.push(line.time);
+		}
+	}
 
-    return index;
-  }
+	updateLine(index: number, line: Omit<Partial<LRCLine>, "id">) {
+		if ("time" in line) line.time = undefined;
 
-  addLine(line: LRCLine, after: number = this.lines.length - 1) {
-    this.lines.splice(after + 1, 0, line);
-    this._cachedTime.splice(after + 1, 0, line.time);
+		Object.assign({}, this.lines[index], line);
+		this._cachedTime[index] = this.lines[index].time;
 
-    this.dispatchEvent('line-added', [after + 1, line]);
-  }
+		this.dispatchEvent("line-updated", index, this.lines[index]);
+	}
 
-  timeToString(time: number, timeFixed: 2 | 3 = 2) {
-    const mins = Math.floor(time / 60000);
-    const secs = Math.floor((time / 1000) % 60);
-    const msec = Math.floor(time) % 1000;
+	removeLine(index: number) {
+		const line = this.lines[index];
+		this.lines.splice(index, 1);
+		this._cachedTime.splice(index, 1);
 
-    const minutes = mins.toString().padStart(2, '0');
-    const seconds = secs.toString().padStart(2, '0');
-    const milsecs = msec.toString().padStart(3, '0').slice(0, timeFixed);
+		this.dispatchEvent("line-removed", index, line);
+	}
 
-    return `${minutes}:${seconds}.${milsecs}`;
-  }
+	removeFromId(id: number) {
+		const index = this.lines.findIndex((line) => line.id === id);
 
-  timeToNumber(time: string) {
-    const [mins, secs, msec] = time.split(/[\:\.]/);
-    const ms = msec.padEnd(3, '0').slice(0, 3);
+		if (index !== -1) {
+			this.removeLine(index);
+		}
 
-    const minutes = parseInt(mins) * 60 * 1000;
-    const seconds = parseInt(secs) * 1000;
-    const milsecs = parseInt(ms);
+		return index;
+	}
 
-    return minutes + seconds + milsecs;
-  }
+	addLine(line: LRCLine, after: number = this.lines.length - 1) {
+		this.lines.splice(after + 1, 0, line);
+		this._cachedTime.splice(after + 1, 0, line.time);
 
-  findIndexHelper(index: number): number {
-    if (index < 0) return -1;
-    const line = this.lines[index];
-    const prev = this.lines[index - 1];
-    if (!prev || prev.time < line.time) return index;
-    return this.findIndexHelper(index - 1);
-  }
+		this.dispatchEvent("line-added", after + 1, line);
+	}
 
-  findIndex(timestamp: number, useCache = true) {
-    const array = useCache ? this._cachedTime : this.lines;
-    for (let i = 0; i < array.length; i++) {
-      const line = this.lines[i];
-      const item = array[i];
-      const time = typeof item == 'number' ? item : line.time;
-      if (time > timestamp) {
-        return this.findIndexHelper(i - 1);
-      }
-    }
-    return this.findIndexHelper(this.lines.length - 1);
-  }
+	timeToString(time: number, timeFixed: 2 | 3 = 2) {
+		const mins = Math.floor(time / 60000);
+		const secs = Math.floor((time / 1000) % 60);
+		const msec = Math.floor(time) % 1000;
 
-  currentLine(timestamp: number): LRCLine | undefined {
-    const index = this.findIndex(timestamp);
-    return this.lines[index];
-  }
+		const minutes = String(mins).padStart(2, "0");
+		const seconds = String(secs).padStart(2, "0");
+		const milsecs = String(msec).padStart(3, "0").slice(0, timeFixed);
 
-  previousLine(timestamp: number): LRCLine | undefined {
-    const index = this.findIndex(timestamp);
-    if (index <= 0) return undefined;
-    return this.lines[index - 1];
-  }
+		return `${minutes}:${seconds}.${milsecs}`;
+	}
 
-  nextLine(timestamp: number): LRCLine | undefined {
-    const index = this.findIndex(timestamp);
-    return this.lines[index + 1];
-  }
+	public static timeToInt(time: string) {
+		const [mins, secs, msec] = time.split(/[\:\.]/);
+		const ms = msec.padEnd(3, "0").slice(0, 3);
 
-  reset() {
-    this.tags = {};
-    this.lines.length = 0;
-    this._cachedTime.length = 0;
-  }
+		const minutes = Number.parseInt(mins) * 60 * 1000;
+		const seconds = Number.parseInt(secs) * 1000;
+		const milsecs = Number.parseInt(ms);
+
+		return minutes + seconds + milsecs;
+	}
+
+	findIndexHelper(index: number): number {
+		if (index < 0) return -1;
+		const line = this.lines[index];
+		const prev = this.lines[index - 1];
+		if (!prev || prev.time < line.time) return index;
+		return this.findIndexHelper(index - 1);
+	}
+
+	findIndex(timestamp: number, useCache = true) {
+		const array = useCache ? this._cachedTime : this.lines;
+		for (let i = 0; i < array.length; i++) {
+			const item = array[i];
+			const line = this.lines[i];
+			const time = is(item, "number") ? item : line.time;
+
+			if (time > timestamp) return this.findIndexHelper(i - 1);
+		}
+		return this.findIndexHelper(this.lines.length - 1);
+	}
+
+	currentLine(timestamp: number): LRCLine | undefined {
+		const index = this.findIndex(timestamp);
+		return this.lines[index];
+	}
+
+	previousLine(timestamp: number): LRCLine | undefined {
+		const index = this.findIndex(timestamp);
+		if (index <= 0) return undefined;
+		return this.lines[index - 1];
+	}
+
+	nextLine(timestamp: number): LRCLine | undefined {
+		const index = this.findIndex(timestamp);
+		return this.lines[index + 1];
+	}
+
+	reset() {
+		this.tags = {};
+		this.lines.length = 0;
+		this._cachedTime.length = 0;
+	}
 }
 
 export default LRCParser;
