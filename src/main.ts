4a5c17f96e28a5af63227543ca8aef81225ebf8e
obillekyle <obillekyle@gmail.com>
2024-10-13 22:38:40 +0800

    perf: remove unused startup code

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,11 @@
-import './global';
-import './assets/main.scss';
-import './assets/markdown.scss';
-import './assets/formatter.scss';
+import "./global";
+import "./assets/main.scss";
+import "./assets/markdown.scss";
+import "./assets/formatter.scss";
 
-import App from './App.vue';
-import { createApp } from 'vue';
-import modalPresets from './components/modals/_presets';
-import { AppScreens } from './app/main';
+import App from "./App.vue";
+import { createApp } from "vue";
+import modalPresets from "./components/modals/presets";
+import { AppScreens } from "./app/main";
 
-createApp(App).mount('#app');
-
-const Options = window.app.options;
-const Lang = window.app.i18n;
-
-function init() {
-  if (Options.get('version') !== window.app.version) {
-    Options.set('version', window.app.version);
-    console.log('initial');
-  } else if (Options.get('showChangeLog')) {
-    modalPresets.changelog();
-  }
-
-  const urlParams = new URLSearchParams(window.location.search);
-  const lyrics = urlParams.get('l');
-  const screen = urlParams.get('s');
-
-  if (lyrics) {
-    modalPresets.useQueryLRC(lyrics);
-  }
-
-  if (screen) {
-    const screenValue = AppScreens[Number(screen)];
-    screenValue && window.app.screen.set(screenValue);
-  }
-
-  if (location.pathname.startsWith('/lyric-card')) {
-    console.log('lyric-card');
-  } else if (location.pathname.startsWith('/test')) {
-    console.log('test');
-  } else {
-    window.history.replaceState({}, document.title, '/');
-  }
-
-  Lang.removeEventListener('ready', init);
-}
-
-Lang.addEventListener('ready', init);
+createApp(App).mount("#app");
