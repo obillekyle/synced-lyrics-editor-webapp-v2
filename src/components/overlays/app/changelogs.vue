d5b90f1a629801b8302b4cd8915406a032ddbc2f
obillekyle <obillekyle@gmail.com>
2024-10-18 21:23:07 +0800

    fix(app/overlays): `changelogs` use fetch.data for markdown parsing

--- src/components/overlays/app/changelogs.vue
+++ src/components/overlays/app/changelogs.vue
@@ -3,15 +3,11 @@ import { useFetch } from '@/hooks/use-fetch'
 import { Button, LinearProgress } from '@vue-material/core'
 import { parse } from 'marked'
 
-const fetch = useFetch('/changelog.md')
-
-defineProps<{
-	data?: string
-}>()
+const fetch = useFetch('/changelog.md', 'text')
 </script>
 
 <template>
   <LinearProgress v-if="fetch.loading" />
   <Button v-else-if="fetch.error" @click="fetch.refetch">Reload</Button>
-  <div v-else v-html="parse(data || 'No data')" class="changelog marked-content" />
+  <div v-else v-html="parse(fetch.data || 'No data')" class="changelog marked-content" />
 </template>
