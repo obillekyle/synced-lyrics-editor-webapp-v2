cc7f568469431d9914510fda51f8d2bf4d3cd937
obillekyle <obillekyle@gmail.com>
2024-10-25 23:44:59 +0800

    refactor(hooks): update and remove existing hooks from lib

--- src/hooks/use-app-data.ts
+++ src/hooks/use-app-data.ts
@@ -1,18 +1,18 @@
-import type { LRCLine } from '@/api/parser'
-import { useIDBStorage } from '@vue-material/core'
+import type { LRCData, LRCLine } from '@/api/parser2'
+import { toProxy, useIDBStorage } from '@vue-material/core'
 import { inject, provide } from 'vue'
 
 type AppData = {
-	lyrics: LRCLine[]
+	lrc: LRCData
 	audio?: Blob
 }
 
 export function useAppData() {
 	const data = inject(
 		'app-data',
-		useIDBStorage<AppData>('app-data', { lyrics: [] }),
+		useIDBStorage<AppData>('app-data', { lrc: { lines: {}, tags: {} } }),
 	)
 
 	provide('app-data', data)
-	return data
+	return toProxy(data)
 }
